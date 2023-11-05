521ffcab961f904959b24601ce1598d76952312a
Gianluca <gianluca@goautonomous.io>
2023-11-05 14:23:30 +0100

    Firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,9 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-require("dotenv").config()
+require("dotenv").config({
+    path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
     siteMetadata: {
@@ -11,14 +13,31 @@ module.exports = {
     plugins: [
         "gatsby-plugin-styled-components",
         "gatsby-plugin-image",
-        "gatsby-plugin-sitemap", {
+        "gatsby-plugin-sitemap",
+        {
             resolve: 'gatsby-plugin-manifest',
             options: {
                 "icon": "src/images/icon.png"
             }
         },
         "gatsby-plugin-sharp",
-        "gatsby-transformer-sharp", {
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-plugin-firebase",
+            options: {
+                credentials: {
+                    apiKey: process.env.GATSBY_API_KEY,
+                    authDomain: process.env.GATSBY_AUTH_DOMAIN,
+                    databaseURL: process.env.GATSBY_DATABASE_URL,
+                    projectId: process.env.GATSBY_PROJECT_ID,
+                    storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+                    messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+                    appId: process.env.GATSBY_APP_ID,
+                    measurementId: process.env.GATSBY_MEASUREMENT_ID
+                }
+            }
+        },
+        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 "name": "images",
@@ -33,5 +52,7 @@ module.exports = {
                 "path": "./src/pages/"
             },
             __key: "pages"
-        }]
+        }
+    ],
+
 };
