30734dc2f121bf883d9ac58ad7838603461a0d06
Gianluca <gianluca@goautonomous.io>
2023-11-05 14:25:29 +0100

    Disqus

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,6 +38,12 @@ module.exports = {
             }
         },
         {
+            resolve: `gatsby-plugin-disqus`,
+            options: {
+                shortname: `gianlucabelvisi`
+            }
+        },
+        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 "name": "images",
